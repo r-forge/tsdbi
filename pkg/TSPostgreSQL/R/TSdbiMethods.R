0f87382f8e9d4ef524dbe6fd57ee2be18acbed6f
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-17 02:53:22 +0000

    Initializing TSPostgreSQL package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/TSPostgreSQL/R/TSdbiMethods.R
@@ -0,0 +1,70 @@
+.onLoad <- function(library, section) {
+   require("methods")
+   require("TSdbi")
+   require("RPostgreSQL")
+   }
+
+setClass("TSPostgreSQLConnection", contains=c("PostgreSQLConnection", "TSdb")) 
+
+#setMethod("print", "TSPostgreSQLConnection", function(x, ...) {
+#   print(x@TSdb)
+#   })
+
+setMethod("TSconnect",   signature(drv="PostgreSQLDriver", dbname="character"),
+   definition=function(drv, dbname, host=
+    if(!is.null(Sys.getenv("PGHOST"))) Sys.getenv("PGHOST") else "localhost", ...) {
+        con <- dbConnect(drv, dbname=dbname, host=host, ...)
+	if(0 == length(dbListTables(con))){
+	  dbDisconnect(con)
+          stop("Database ",dbname," has no tables.")
+	  }
+	if(!dbExistsTable(con, "Meta")){
+	  dbDisconnect(con)
+          stop("Database ",dbname," does not appear to be a TS database.")
+	  }
+  	new("TSPostgreSQLConnection" , con, dbname=dbname, 
+ 	       hasVintages=dbExistsTable(con, "vintages"), 
+ 	       hasPanels  =dbExistsTable(con, "panels")) 
+	})
+
+setMethod("TSput",   signature(x="ANY", serIDs="character", con="PostgreSQLConnection"),
+   definition= function(x, serIDs, con=getOption("TSconnection"), Table=NULL, 
+       TSdescription.=TSdescription(x), TSdoc.=TSdoc(x), TSlabel.=TSlabel(x),  
+       vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
+  TSdbi:::TSputSQL(x, serIDs, con, Table=Table, 
+   TSdescription.=TSdescription., TSdoc.=TSdoc., TSlabel.=TSlabel., 
+   vintage=vintage, panel=panel,...) )
+
+setMethod("TSget",   signature(serIDs="character", con="PostgreSQLConnection"),
+   definition= function(serIDs, con=getOption("TSconnection"), 
+       TSrepresentation=getOption("TSrepresentation"),
+       tf=NULL, start=tfstart(tf), end=tfend(tf),
+       names=serIDs, TSdescription=FALSE, TSdoc=FALSE, TSlabel=FALSE,
+       vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
+   TSdbi:::TSgetSQL(serIDs, con, TSrepresentation=TSrepresentation,
+       tf=tf, start=start, end=end,
+       names=names, TSdescription=TSdescription, TSdoc=TSdoc, TSlabel=TSlabel,
+       vintage=vintage, panel=panel, ...) )
+
+setMethod("TSdates",    signature(serIDs="character", con="PostgreSQLConnection"),
+   definition= function(serIDs, con=getOption("TSconnection"),  
+       vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
+      TSdbi:::TSdatesSQL(serIDs, con, vintage=vintage, panel=panel, ...) )
+
+
+setMethod("TSdescription",   signature(x="character", con="PostgreSQLConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...)
+        TSdbi:::TSdescriptionSQL(x=x, con=con, ...) )
+
+setMethod("TSdoc",   signature(x="character", con="PostgreSQLConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...)
+        TSdbi:::TSdocSQL(x=x, con=con, ...) )
+
+setMethod("TSlabel",   signature(x="character", con="PostgreSQLConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...)
+        TSdbi:::TSlabelSQL(x=x, con=con, ...) )
+
+setMethod("TSdelete", signature(serIDs="character", con="PostgreSQLConnection"),
+   definition= function(serIDs, con=getOption("TSconnection"),  
+   vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
+       TSdbi:::TSdeleteSQL(serIDs=serIDs, con=con, vintage=vintage, panel=panel, ...) )
