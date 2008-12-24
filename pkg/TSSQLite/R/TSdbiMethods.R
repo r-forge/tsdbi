9d1f227626513d9751580b3a09c9eaf5a7d7fc34
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2008-12-24 16:22:55 +0000

    Initializing TSSQLite package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/TSSQLite/R/TSdbiMethods.R
@@ -0,0 +1,63 @@
+.onLoad <- function(library, section) {
+   require("methods")
+   require("TSdbi")
+   require("RSQLite")
+   }
+
+setClass("TSSQLiteConnection", contains=c("SQLiteConnection","TSdb")) 
+
+setMethod("print", "TSSQLiteConnection", function(x, ...) {
+    print(x@TSdb)
+    })
+
+setMethod("TSconnect",   signature(drv="SQLiteDriver", dbname="character"),
+   definition=function(drv, dbname, ...) {
+        con <- dbConnect(drv, dbname=dbname, ...)
+	if(!dbExistsTable(con, "Meta"))
+	  stop("The database does not appear to be a TS database,")
+	new("TSSQLiteConnection" , con, dbname=dbname, 
+	       hasVintages=dbExistsTable(con, "vintages"), 
+	       hasPanels  =dbExistsTable(con, "panels"))
+	})
+
+setMethod("TSput", signature(x="ANY", serIDs="character", con="SQLiteConnection"),
+   definition= function(x, serIDs=seriesNames(x), con=getOption("TSconnection"), Table=NULL, 
+       TSdescription.=TSdescription(x), TSdoc.=TSdoc(x),   TSlabel.=TSlabel(x),
+       vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...) 
+ TSdbi:::TSputSQL(x, serIDs, con, Table=Table, 
+  TSdescription.=TSdescription., TSdoc.=TSdoc., TSlabel.=TSlabel., 
+  vintage=vintage, panel=panel, ...) )
+
+setMethod("TSget", signature(serIDs="character", con="SQLiteConnection"),
+   definition= function(serIDs, con=getOption("TSconnection"), 
+       TSrepresentation=options()$TSrepresentation,
+       tf=NULL, start=tfstart(tf), end=tfend(tf),
+       names=serIDs, TSdescription=FALSE, TSdoc=FALSE, TSlabel=FALSE,
+       vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
+    TSdbi:::TSgetSQL(serIDs, con, TSrepresentation=TSrepresentation,
+       tf=tf, start=start, end=end,
+       names=names, TSdescription=TSdescription, TSdoc=TSdoc, TSlabel=TSlabel,
+       vintage=vintage, panel=panel, ...) )
+
+setMethod("TSdates", signature(serIDs="character", con="SQLiteConnection"),
+   definition= function(serIDs, con=getOption("TSconnection"),  
+       vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
+     TSdbi:::TSdatesSQL(serIDs, con, vintage=vintage, panel=panel, ...) )
+
+
+setMethod("TSdescription",   signature(x="character", con="SQLiteConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...)
+        TSdbi:::TSdescriptionSQL(x=x, con=con, ...) )
+
+setMethod("TSdoc",   signature(x="character", con="SQLiteConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...)
+        TSdbi:::TSdocSQL(x=x, con=con, ...) )
+
+setMethod("TSlabel",   signature(x="character", con="SQLiteConnection"),
+   definition= function(x, con=getOption("TSconnection"), ...)
+        TSdbi:::TSlabelSQL(x=x, con=con, ...) )
+
+setMethod("TSdelete", signature(serIDs="character", con="SQLiteConnection"),
+     definition= function(serIDs, con=getOption("TSconnection"),  
+     vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
+  TSdbi:::TSdeleteSQL(serIDs=serIDs, con=con, vintage=vintage, panel=panel, ...) )
