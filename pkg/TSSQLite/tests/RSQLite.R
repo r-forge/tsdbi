f330f49b97d2185041f50667feebb4783ad34475
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-24 16:22:55 +0000

    Initializing TSSQLite package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/TSSQLite/tests/RSQLite.R
@@ -0,0 +1,30 @@
+require("TSSQLite")
+
+if(require("RSQLite") ) {
+cat("************** RSQLite  Examples ******************************\n")
+cat("**************************************************************\n")
+cat("* WARNING: THIS OVERWRITES TABLES IN TEST DATABASE ON SERVER**\n")
+cat("**************************************************************\n")
+
+m <- dbDriver("SQLite")
+
+con <- dbConnect(m, dbname="test") # no user/passwd/host
+
+dbListTables(con) 
+source(system.file("TSsql/CreateTables.TSsql", package = "TSdbi"))
+dbListTables(con) 
+dbDisconnect(con)
+
+con <- try(TSconnect(m, dbname="test") )
+if(inherits(con, "try-error")) stop("CreateTables did not work.")
+
+source(system.file("TSsql/Populate.TSsql", package = "TSdbi"))
+source(system.file("TSsql/TSdbi.TSsql", package = "TSdbi"))
+source(system.file("TSsql/dbGetQuery.TSsql", package = "TSdbi"))
+source(system.file("TSsql/HistQuote.TSsql", package = "TSdbi"))
+
+cat("**************        disconnecting test\n")
+dbDisconnect(con)
+#  dbUnloadDriver(m) complains about open connections.
+
+} else  warning("RSQLite not available. Skipping tests.")
