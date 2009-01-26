514b59e1d230d2eb591bda2d595949e5af020064
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2009-01-26 21:48:11 +0000

    Tests adjusted to check environment variables for user/passwd/host/dbname.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSMySQL/tests/0serviceCheck.R
+++ pkg/TSMySQL/tests/0serviceCheck.R
@@ -1,8 +1,31 @@
-z <- Sys.getenv("_R_CHECK_HAVE_MYSQL_")
+# This just does a dbConnect (not a TSconnect) to see if things work
+
+service <- Sys.getenv("_R_CHECK_HAVE_MYSQL_")
 
 Sys.info()
 
-if(identical(as.logical(z), TRUE))  require("TSMySQL") else {
+if(identical(as.logical(service), TRUE)) {
+   require("TSMySQL") 
+   m <- dbDriver("MySQL")
+
+   dbname   <- Sys.getenv("MYSQL_DATABASE")
+   if ("" == dbname)   dbname <- NULL
+
+   user    <- Sys.getenv("MYSQL_USER")
+   if ("" != user) {
+       # specifying host as NULL or "localhost" results in a socket connection
+       host    <- Sys.getenv("MYSQL_HOST")
+       if ("" == host)     host <- Sys.info()["nodename"] 
+       if ("" == passwd)   passwd <- NULL
+       passwd  <- Sys.getenv("MYSQL_PASSWD")
+       #  See  ?"dbConnect-methods"
+       con <- dbConnect(m,
+          username=user, password=passwd, host=host, dbname=dbname)  
+      }else  con <- 
+       dbConnect(m, dbname=dbname) # pass user/passwd/host in ~/.my.cnf
+   # dbListTables(con) needs a non-null dbname
+   dbDisconnect(con)
+ }else {
    cat("MYSQL not available. Skipping tests.\n")
-   cat("_R_CHECK_HAVE_MYSQL_ setting ", z, "\n")
+   cat("_R_CHECK_HAVE_MYSQL_ setting ", service, "\n")
    }
