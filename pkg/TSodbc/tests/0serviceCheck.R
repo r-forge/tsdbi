167fab7b76934cc24402d271d96e5079e6ba3620
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2009-01-26 22:05:10 +0000

    Tests adjusted to check environment variables for user/passwd/host/dbname.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSodbc/tests/0serviceCheck.R
+++ pkg/TSodbc/tests/0serviceCheck.R
@@ -1,8 +1,27 @@
-z <- Sys.getenv("_R_CHECK_HAVE_ODBC_")
+# This just does a dbConnect (not a TSconnect) to see if things work
+
+service <- Sys.getenv("_R_CHECK_HAVE_ODBC_")
 
 Sys.info()
 
-if(identical(as.logical(z), TRUE))  require("TSodbc") else {
+if(identical(as.logical(service), TRUE)) {
+   require("TSodbc") 
+
+   dbname   <- Sys.getenv("ODBC_DATABASE")
+   if ("" == dbname)   dbname <- NULL
+
+   user    <- Sys.getenv("ODBC_USER")
+   if ("" != user) {
+       host    <- Sys.getenv("ODBC_HOST")
+       if ("" == host)     host <- Sys.info()["nodename"] 
+       if ("" == passwd)   passwd <- NULL
+       passwd  <- Sys.getenv("ODBC_PASSWD")
+       #  See  ?odbcConnect   ?odbcDriverConnect
+       con <- odbcConnect(dsn=dbname, uid="user, pwd=passwd, connection=host))  
+     }else  con <- 
+       odbcConnect(dsn=dbname) # pass user/passwd/host in ~/.odbc.ini
+   dbDisconnect(con)
+ }else {
    cat("ODBC not available. Skipping tests.\n")
-   cat("_R_CHECK_HAVE_ODBC_ setting ", z, "\n")
+   cat("_R_CHECK_HAVE_ODBC_ setting ", service, "\n")
    }
--- pkg/TSodbc/tests/Rodbc.R
+++ pkg/TSodbc/tests/Rodbc.R
@@ -1,32 +1,45 @@
-# Before starting R you need to set user/passwd/host etc  in ~/.odbc.ini
-if(identical(as.logical(Sys.getenv("_R_CHECK_HAVE_ODBC_")), TRUE)) {
+service <- Sys.getenv("_R_CHECK_HAVE_ODBC_")
+
+if(identical(as.logical(service), TRUE)) {
 
 require("TSodbc")
-require("RODBC") 
-require("tframe")
 
 cat("************** RODBC  Examples ******************************\n")
 cat("**************************************************************\n")
 cat("* WARNING: THIS OVERWRITES TABLES IN TEST DATABASE ON SERVER**\n")
 cat("**************************************************************\n")
 
-m <- dbDriver("ODBC")
-
 ###### This is to set up tables. Otherwise use TSconnect#########
   # This will fail if ODBC support is not installed or set up on the system 
   # with a message like:  [RODBC] ERROR: state IM002, code 0, 
   # message [unixODBC][Driver Manager]Data source name not found, 
   # and no default driver specified
- con <- odbcConnect("test") # pass user/ passwd / host in .odbc.ini
-##################################################################
+
+   dbname   <- Sys.getenv("ODBC_DATABASE")
+   if ("" == dbname)   dbname <- "test"
+
+   user    <- Sys.getenv("ODBC_USER")
+   if ("" != user) {
+       host    <- Sys.getenv("ODBC_HOST")
+       if ("" == host)     host <- Sys.info()["nodename"] 
+       if ("" == passwd)   passwd <- NULL
+       passwd  <- Sys.getenv("ODBC_PASSWD")
+       #  See  ?odbcConnect   ?odbcDriverConnect
+       con <- odbcConnect(dsn=dbname, uid="user, pwd=passwd, connection=host))  
+     }else  con <- 
+       odbcConnect(dsn=dbname) # pass user/passwd/host in ~/.odbc.ini
+   dbDisconnect(con)
 
 #dbListTables(con) 
 source(system.file("TSsql/CreateTables.TSsql", package = "TSdbi"))
 #dbListTables(con) 
 #dbDisconnect(con)
+##################################################################
 
-#con <- TSconnect("ODBC", dbname="test") 
-con <- tryCatch(TSconnect("ODBC", dbname="test"))
+con <- if ("" != user)  
+          tryCatch(TSconnect("ODBC", dbname=dbname, username=user, password=passwd, host=host)) 
+    else  tryCatch(TSconnect("ODBC", dbname=dbname)) # pass user/passwd/host in ~/.my.cnf
+
 if(inherits(con, "try-error")) stop("CreateTables did not work.")
 
 source(system.file("TSsql/Populate.TSsql", package = "TSdbi"))
