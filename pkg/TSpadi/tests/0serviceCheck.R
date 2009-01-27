b42ea00ec1dd33d4b638212081c40c1ab80a14e6
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2009-01-27 21:28:45 +0000

    added test TSpadi.R, and now checking environment variables for user/password/host, before using file .padi.cfg.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSpadi/tests/0serviceCheck.R
+++ pkg/TSpadi/tests/0serviceCheck.R
@@ -1,8 +1,29 @@
-z <- Sys.getenv("_R_CHECK_HAVE_PADI_")
+# This does a TSconnect to see if things work
+
+service <- Sys.getenv("_R_CHECK_HAVE_PADI_")
 
 Sys.info()
 
-if(identical(as.logical(z), TRUE))  require("TSpadi") else {
+if(identical(as.logical(service), TRUE)) {
+   require("TSpadi") 
+   m <- dbDriver("padi")
+
+   dbname   <- Sys.getenv("PADI_DATABASE")
+   if (is.null(dbname))   dbname <- "test"  
+
+   user    <- Sys.getenv("PADI_USER")
+   if ("" != user) {
+       # specifying host as NULL or "localhost" results in a socket connection
+       host    <- Sys.getenv("PADI_HOST")
+       if ("" == host)     host <- Sys.info()["nodename"] 
+       if ("" == passwd)   passwd <- NULL
+       passwd  <- Sys.getenv("PADI_PASSWD")
+       con <- TSconnect(m,
+          username=user, password=passwd, host=host, dbname=dbname)  
+      }else {
+	con <- TSconnect(m, dbname=dbname) # pass user/passwd/host in .padi.cfg
+	}
+ }else {
    cat("PADI not available. Skipping tests.\n")
-   cat("_R_CHECK_HAVE_PADI_ setting ", z, "\n")
+   cat("_R_CHECK_HAVE_PADI_ setting ", service, "\n")
    }
