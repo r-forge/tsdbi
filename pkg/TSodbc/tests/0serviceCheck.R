6b25506a8eb8853c35fb9f2c6b4741cae8b998d9
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-27 03:11:45 +0000

    fixed TSodbc tests to use environment variable for user/paswd/host, or .odbc.ini in environment variables are not set.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSodbc/tests/0serviceCheck.R
+++ pkg/TSodbc/tests/0serviceCheck.R
@@ -17,11 +17,13 @@ if(identical(as.logical(service), TRUE)) {
        if ("" == passwd)   passwd <- NULL
        passwd  <- Sys.getenv("ODBC_PASSWD")
        #  See  ?odbcConnect   ?odbcDriverConnect
-       con <- odbcConnect(dsn=dbname, uid="user, pwd=passwd, connection=host))  
-     }else  con <- 
-       odbcConnect(dsn=dbname) # pass user/passwd/host in ~/.odbc.ini
-   dbDisconnect(con)
- }else {
+       con <- odbcConnect(dsn=dbname, uid=user, pwd=passwd, connection=host)  
+    } else {
+       if (is.null(dbname))   dbname <- "test"  # NULL dbname causes problem
+       con <-  odbcConnect(dsn=dbname) # pass user/passwd/host in ~/.odbc.ini
+       }
+   odbcClose(con)
+ } else {
    cat("ODBC not available. Skipping tests.\n")
    cat("_R_CHECK_HAVE_ODBC_ setting ", service, "\n")
    }
