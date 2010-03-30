8d3944ee5888e7744a7dc624212c6fb2f0a66058
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2010-03-30 13:30:46 +0000

    fixed bug passing names to generic TSget (always used serIDs). Bumped up version numbers and NEWS.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@115 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSPostgreSQL/R/TSdbiMethods.R
+++ pkg/TSPostgreSQL/R/TSdbiMethods.R
@@ -40,7 +40,7 @@ setMethod("TSget",   signature(serIDs="character", con="PostgreSQLConnection"),
    definition= function(serIDs, con=getOption("TSconnection"), 
        TSrepresentation=getOption("TSrepresentation"),
        tf=NULL, start=tfstart(tf), end=tfend(tf),
-       names=serIDs, TSdescription=FALSE, TSdoc=FALSE, TSlabel=FALSE,
+       names=NULL, TSdescription=FALSE, TSdoc=FALSE, TSlabel=FALSE,
        vintage=getOption("TSvintage"), panel=getOption("TSpanel"), ...)
    TSdbi:::TSgetSQL(serIDs, con, TSrepresentation=TSrepresentation,
        tf=tf, start=start, end=end,
