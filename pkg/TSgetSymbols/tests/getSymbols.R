b3c01a84560fe43baa33bf7292afc5651d45c8ed
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-08 02:44:13 +0000

    rough sketch of TSgetSymbols, not yet working.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@149 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/TSgetSymbols/tests/getSymbols.R
@@ -0,0 +1,57 @@
+require("TSgetSymbols")
+cat("************** TSgetSymbols  Examples ******************************\n")
+
+con <- TSconnect("getSymbols", dbname="FRED") 
+getSymbols('CPIAUCNS',src='FRED')
+
+
+x <- TSget("CPIAUCNS", con) 
+plot(x)
+tfplot(x)
+
+# debugging info
+str(x)
+TSrefperiod(x)
+
+if ("Close" != TSrefperiod(x)) stop("TSrefperiod error, test 1.") 
+TSdescription(x) 
+
+x2 <- TSget("CPIAUCNS", con)
+tfplot(x2)
+plot(x2)
+if ("Close" != TSrefperiod(x2)) stop("TSrefperiod error, test 2.") 
+TSdescription(x2) 
+
+options(TSconnection=con)
+
+x <- TSget(c("^ftse","^gspc"))
+plot(x)
+tfplot(x)
+if (! all(TSrefperiod(x) %in% "Close")) stop("TSrefperiod error, test 3.")
+TSdescription(x) 
+
+x <- TSget("ibm", quote = c("Close", "Vol"))
+plot(x)
+tfplot(x)
+if(!all(TSrefperiod(x) == c("Close", "Vol"))) stop("TSrefperiod error, test 4.")
+TSdescription(x) 
+
+tfplot(x, xlab = TSdescription(x))
+tfplot(x, Title="IBM", start="2007-01-01")
+
+conO <- TSconnect("getSymbols", dbname="yahoo") 
+
+# oanda has max 500 data points
+z <- TSget("EUR/USD", conO, start=Sys.Date() - 495)
+plot(z)
+tfplot(z)
+if ("Close" != TSrefperiod(z)) stop("TSrefperiod error, test 5.") 
+
+tfplot(z, Title = "EUR/USD")
+tfplot(z, Title = "EUR/USD", start="2007-01-01")
+tfplot(z, Title = "EUR/USD", start="2007-03-01")
+tfplot(z, Title = "EUR/USD", start=Sys.Date()-14, end=Sys.Date(),
+   xlab = format(Sys.Date(), "%Y"))
+
+
+TSdates(c("^ftse","^gspc", "ibm"), con) # note default start
