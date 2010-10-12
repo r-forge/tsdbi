f77d046179089fa53e54287e54274cb024ca5319
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-12 12:06:26 +0000

    partly working but not right frequency, not building, and not documented.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@150 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/TSgetSymbol/tests/getSymbol.R
@@ -0,0 +1,52 @@
+require("TSgetSymbol")
+cat("************** TSgetSymbol  Examples ******************************\n")
+
+con <- TSconnect("getSymbol", dbname="FRED") 
+
+x <- TSget("CPIAUCNS", con) 
+plot(x)
+tfplot(x)
+
+# debugging info
+str(x)
+
+TSdescription(x) 
+
+options(TSconnection=con)
+
+x2 <- TSget("M2")
+tfplot(x2)
+plot(x2)
+TSdescription(x2) 
+
+x <- TSget(c("CPIAUCNS","M2"), con)
+plot(x)
+tfplot(x)
+TSdescription(x) 
+
+x <- TSget(c("TOTALSL","TOTALNS"), con, 
+       names=c("Total Consumer Credit Outstanding SA",
+               "Total Consumer Credit Outstanding NSA"))
+plot(x)
+tfplot(x)
+TSdescription(x) 
+
+Q dates on these are month-day, and frequency is wrong
+
+x <- TSget(c("TDSP","FODSP"), con, 
+       names=c("Household Debt Service Payments as a Percent of Disposable Personal Income",
+               "Household Financial Obligations as a percent of Disposable Personal Income"))
+tfplot(x)
+TSdescription(x) 
+
+x <- TSget("ibm", quote = c("Close", "Vol"))
+plot(x)
+tfplot(x)
+if(!all(TSrefperiod(x) == c("Close", "Vol"))) stop("TSrefperiod error, test 4.")
+TSdescription(x) 
+
+tfplot(x, xlab = TSdescription(x))
+tfplot(x, Title="IBM", start="2007-01-01")
+
+conO <- TSconnect("getSymbol", dbname="yahoo") 
+
