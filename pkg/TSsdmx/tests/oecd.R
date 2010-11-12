f8ccf09d567b14a045f2c9663ed1d390222b422b
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-12 03:11:03 +0000

    ongoing, not working.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@174 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSsdmx/tests/oecd.R
+++ pkg/TSsdmx/tests/oecd.R
@@ -13,6 +13,7 @@ Yes, it's using SOAP. There's a link on this page --
  http://stats.oecd.org/SDMXWS/sdmx.asmx
 to the "service description", which leads to the WSDL file that describes the 
 web service: http://stats.oecd.org/SDMXWS/sdmx.asmx?WSDL
+web service: http://stats.oecd.org/OECDSTATWS_SDMXNEW/sdmx.asmx?WSDL
 
  
 You might find it helpful to feed that WSDL (Web Service Definition Language) 
@@ -123,7 +124,10 @@ require("RCurl")
 require("RCurl")
 require("XML")
 
-soapenv1 <- '<?xml version="1.0" encoding="UTF-8"?>\
+# see example at http://stats.oecd.org/SDMXWS/sdmx.asmx?op=GetGenericData
+
+#soap1.0
+soap1.0.env.head <- '<?xml version="1.0" encoding="UTF-8"?>\
      <SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" \
                         xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" \
                         xmlns:xsd="http://www.w3.org/1999/XMLSchema" \
@@ -131,9 +135,136 @@ soapenv1 <- '<?xml version="1.0" encoding="UTF-8"?>\
                         xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance">\
        <SOAP-ENV:Body>\ '
 
-soapenv2 <- '</SOAP-ENV:Body>\
+soap1.0.env.foot <- '</SOAP-ENV:Body>\
      </SOAP-ENV:Envelope>\n'
 
+#soap1.1
+# note SOAPAction: "http://stats.oecd.org/OECDStatWS/SDMX/GetGenericData"
+# is in the HTML header befor the soap envelope
+soap1.1.env.head <- '<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>'
+#    <GetGenericData xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+#      <QueryMessage>
+
+#</QueryMessage>
+#    </GetGenericData>
+soap1.1.env.foot <- '
+  </soap:Body>
+</soap:Envelope>'
+
+#soap1.2
+# note no SOAPAction
+soap1.2.env.head <- '<?xml version="1.0" encoding="utf-8"?>
+<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>'
+#    <GetGenericData xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+#      <QueryMessage>
+
+#</QueryMessage>
+#    </GetGenericData>
+soap1.2.env.foot <- '
+  </soap12:Body>
+</soap12:Envelope>'
+
+# example from http://stats.oecd.org/SDMXWS/QueryPage.aspx?Type=DataGeneric
+# works  with Get Data button on oecd site 
+# (but trouble loading autoamtically in gedit)
+queryMessageEg <- '
+<message:QueryMessage xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query" xmlns:message="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message" xsi:schemaLocation="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query http://www.sdmx.org/docs/2_0/SDMXQuery.xsd http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message http://www.sdmx.org/docs/2_0/SDMXMessage.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+	<Header xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message">
+		<ID>none</ID>
+		<Test>false</Test>
+		<Truncated>false</Truncated>
+		<Prepared>2010-11-11T18:35:38</Prepared>
+		<Sender id="YourID">
+			<Name xml:lang="en">Your English Name</Name>
+		</Sender>
+		<Receiver id="OECD">
+			<Name xml:lang="en">Organisation for Economic Co-operation and Development</Name>
+			<Name xml:lang="fr">Organisation de coopération et de développement économiques</Name>
+		</Receiver>
+		<!--
+    <message:DataSetAction>Replace</message:DataSetAction>
+    <message:ReportingBegin>2007-02-22T00:00:00</message:ReportingBegin>
+    -->
+	</Header>
+	<Query xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message">
+		<DataWhere xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query">
+			<And>
+				<DataSet>REFSERIES</DataSet>
+				<!--Dimension id="LOCATION">AUS</Dimension-->
+				<Attribute id="TIME_FORMAT">P1Y</Attribute>
+				<Time>
+					<StartTime>1960</StartTime>
+					<EndTime>2010</EndTime>
+				</Time>
+				<Or>
+					<Dimension id="SUBJECT">DEMOPOP_0</Dimension>
+					<Dimension id="SUBJECT">YP</Dimension>
+					<Dimension id="SUBJECT">YPTTTTL1_ST</Dimension>
+					<Dimension id="SUBJECT">STATFIN_0</Dimension>
+					<Dimension id="SUBJECT">IR</Dimension>
+					<Dimension id="SUBJECT">IR3T</Dimension>
+					<Dimension id="SUBJECT">IRLT</Dimension>
+					<Dimension id="SUBJECT">STATFIN_06</Dimension>
+					<Dimension id="SUBJECT">SP</Dimension>
+					<Dimension id="SUBJECT">BP</Dimension>
+					<Dimension id="SUBJECT">BPBLTT01</Dimension>
+					<Dimension id="SUBJECT">CC</Dimension>
+					<Dimension id="SUBJECT">CCUS</Dimension>
+					<Dimension id="SUBJECT">STATFIN_12</Dimension>
+					<Dimension id="SUBJECT">XF</Dimension>
+					<Dimension id="SUBJECT">INDSER_0</Dimension>
+					<Dimension id="SUBJECT">PR</Dimension>
+					<Dimension id="SUBJECT">IPI</Dimension>
+					<Dimension id="SUBJECT">TR_0</Dimension>
+					<Dimension id="SUBJECT">TR_01</Dimension>
+					<Dimension id="SUBJECT">EXPGOOD</Dimension>
+					<Dimension id="SUBJECT">IMPGOOD</Dimension>
+					<Dimension id="SUBJECT">NETGOOD</Dimension>
+					<Dimension id="SUBJECT">LFS_0</Dimension>
+					<Dimension id="SUBJECT">LFS_01</Dimension>
+					<Dimension id="SUBJECT">POPACT</Dimension>
+					<Dimension id="SUBJECT">ET</Dimension>
+					<Dimension id="SUBJECT">EC</Dimension>
+					<Dimension id="SUBJECT">UNEMP</Dimension>
+					<Dimension id="SUBJECT">UNRTSD</Dimension>
+					<Dimension id="SUBJECT">UNRTSDTT</Dimension>
+					<Dimension id="SUBJECT">NA_0</Dimension>
+					<Dimension id="SUBJECT">NA_01</Dimension>
+					<Dimension id="SUBJECT">GDPA</Dimension>
+					<Dimension id="SUBJECT">GDPVA</Dimension>
+					<Dimension id="SUBJECT">PGDPA</Dimension>
+					<Dimension id="SUBJECT">GDPA_PPP</Dimension>
+					<Dimension id="SUBJECT">GDPA_DOLLAR</Dimension>
+					<Dimension id="SUBJECT">GDPA_PH_PPP</Dimension>
+					<Dimension id="SUBJECT">GDPA_PH_DOLLAR</Dimension>
+					<Dimension id="SUBJECT">GDPA_PH_OECD</Dimension>
+					<Dimension id="SUBJECT">NA_02</Dimension>
+					<Dimension id="SUBJECT">GDPQ</Dimension>
+					<Dimension id="SUBJECT">GDPVQ</Dimension>
+					<Dimension id="SUBJECT">PGDPQ</Dimension>
+					<Dimension id="SUBJECT">NA_03</Dimension>
+					<Dimension id="SUBJECT">GDP_EO</Dimension>
+					<Dimension id="SUBJECT">GDPV_EO</Dimension>
+					<Dimension id="SUBJECT">PGDP_EO</Dimension>
+					<Dimension id="SUBJECT">PCP_EO</Dimension>
+					<Dimension id="SUBJECT">PRPPP_0</Dimension>
+					<Dimension id="SUBJECT">CP</Dimension>
+					<Dimension id="SUBJECT">CPITOT</Dimension>
+					<Dimension id="SUBJECT">CPILF</Dimension>
+					<Dimension id="SUBJECT">PP</Dimension>
+					<Dimension id="SUBJECT">PPI</Dimension>
+					<Dimension id="SUBJECT">PRPPP_02</Dimension>
+					<Dimension id="SUBJECT">PPPGDP</Dimension>
+					<Dimension id="SUBJECT">PPPPC</Dimension>
+				</Or>
+			</And>
+		</DataWhere>
+	</Query>
+</message:QueryMessage>'
+	
 queryMessage <- '
 <message:QueryMessage xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query" xmlns:message="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message" xsi:schemaLocation="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query http://www.sdmx.org/docs/2_0/SDMXQuery.xsd http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message http://www.sdmx.org/docs/2_0/SDMXMessage.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 	<Header xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message">
@@ -164,7 +295,6 @@ queryMessage <- '
 					<EndTime>2010</EndTime>
 				</Time>
 				<Or>
-					<Dimension id="SUBJECT">DEMOPOP_0</Dimension>
 					<Dimension id="SUBJECT">IPI</Dimension>
 					<Dimension id="SUBJECT">IMPGOOD</Dimension>
 				</Or>
@@ -173,21 +303,28 @@ queryMessage <- '
 	</Query>
 </message:QueryMessage>'
 			
-body <- paste(soapenv1, queryMessage,soapenv2, collapse="")
 
 soecd <-
-   "http://stats.oecd.org/OECDSTATWS_SDMXNEW/QueryPage.aspx?Type=DataGeneric"
+   "http://stats.oecd.org/OECDSTATWS_SDMXNEW/sdmx.aspx?op=GetGenericData"
 soecd <-
    "http://stats.oecd.org/SDMXWS/sdmx.asmx?op=GetGenericData"
  
 h = basicTextGatherer()
+
+SOAPAction  below may be for wrong version? 
+<wsdl:operation name="GetGenericData">
+<soap12:operation soapAction="http://stats.oecd.org/OECDStatWS/SDMX/GetGenericData" 
+
+body <- paste(soap1.0.env.head, queryMessage,soap1.0.env.foot, collapse="")
+body <- paste(soap1.1.env.head, queryMessage,soap1.1.env.foot, collapse="")
+#<faultstring>Server was unable to process request. ---&gt; Object reference not set to an instance of an object.</faultstring>
+body <- paste(soap1.2.env.head, queryMessageEg,soap1.2.env.foot, collapse="")
+body <- paste(soap1.2.env.head, queryMessage,soap1.2.env.foot, collapse="")
+
 h$reset()
-
-SOAPAction needs to be fixed below
-
 curlPerform(url=soecd,
        httpheader=c(Accept="text/xml", Accept="multipart/*",        
-       SOAPAction='"http://stats.oecd.org/OECDSTATWS_SDMXNEW/QueryPage.aspx?Type=DataGeneric"',       
+       SOAPAction='http://stats.oecd.org/OECDStatWS/SDMX/GetGenericData',
        'Content-Type' = "text/xml; charset=utf-8"),
        postfields=body,
        writefunction = h$update,
@@ -195,6 +332,7 @@ curlPerform(url=soecd,
        )
      
 h$value()
+
 xmlTreeParse(h$value(), asText=TRUE, trim=TRUE)
 str(xmlTreeParse(h$value(), asText=TRUE))
 
