8d2a0d3ff61e94f431ba2a67322a7b6d137599e8
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-14 14:11:51 +0000

    ongoing development, still not working.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@191 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSxls/R/TSdbiMethods.R
+++ pkg/TSxls/R/TSdbiMethods.R
@@ -9,7 +9,9 @@ xls <- function() {
 
 # require("DBI") for this
 setClass("TSxlsConnection", contains=c("DBIConnection", "conType","TSdb"),
-   representation(user="character", password="character", host="character") )
+   representation(url="character", data="matrix", ids="character", 
+        dates="character", names="character", description="character") 
+   )
 
 ####### some kludges to make this look like DBI. ######
 # this does nothing but prevent errors if it is called. 
@@ -18,23 +20,116 @@ setMethod("dbDisconnect", signature(conn="TSxlsConnection"),
 #######     end kludges   ######
 
 setMethod("TSconnect",   signature(drv="xlsDriver", dbname="character"),
-  definition= function(drv, dbname, user="", password="", host="", ...){
+  definition= 
+TSconnectXLS <- function(drv, dbname, 
+     map=list(ids, data, dates, names=NULL, description=NULL,
+              tsrepresentation = function(data,dates){
+		       zoo(data, as.Date(dates))}), ...){
    #  user / password / host  for future consideration
    if (is.null(dbname)) stop("dbname must be specified")
 
-   # there could be a better connection test mechanism below
-   if (dbname == "ECB" )      con <- try(TSgetECB('CPIAUCNS',...),  silent=TRUE)
-   else if (dbname == "OECD") con <- try(TSgetOECD('CPIAUCNS',...), silent=TRUE)
-   else stop(dbname, "not recognized. dbname should be one of 'ECB', 'OECD'.")
+   if (!file.exists(dbname)){
+     url <- dbname
+     zz <- try(sys.call(paste("wget", url)),  silent=TRUE)
+     if(inherits(zz, "try-error")) 
+         stop("Could not find file or url ",  dbname)
+     dbname <-temp.file()
+     file.create(dbname, showWarnings = TRUE)
+     #unlink on R exit or dbdisconnect
+     }
+   else url <- ""
 
-   if(inherits(con, "try-error")) 
+   require("gdata")
+
+   zz <- try(read.xls(dbname, sheet = 1, verbose=FALSE),  silent=TRUE)
+                   #method=c("csv","tsv","tab"), perl="perl")
+   if(inherits(zz, "try-error")) 
          stop("Could not establish TSxlsConnection to ",  dbname)
-   
-   new("TSxlsConnection", drv="xls", dbname=dbname, 
-        hasVintages=FALSE, hasPanels=FALSE, 
-	user=user, password=password, host=host ) 
-   } )
 
+   #NB The first line provides data frame names, so rows are shifted. 
+   #   This fixes so matrix corresponds to spreadsheet cells
+   z <- rbind(names(zz), as.matrix(zz))
+
+   #   Blank rows seem to be skipped, so result is compressed
+
+   # translate cell letters to numbers MAKE LONGER
+   jmap <- function(cols){ 
+        charmatch(sub(":[A-Z]*","",cols), LETTERS):
+        charmatch(sub("[A-Z]*:","",cols), LETTERS) 
+	}
+
+   ids   <- z[map$ids$i,  jmap(map$ids$j)] 
+   data  <- z[map$data$i, jmap(map$data$j)]   
+   dates <- z[map$dates$i,jmap(map$dates$j)]   
+   nm    <- if(is.null(map$names)) NULL else combineRows(
+            z[map$names$i,jmap(map$names$j)]) 
+   desc  <- if(is.null(map$description)) NULL else combineRows(
+            z[map$description$i,jmap(map$description$j)]) 
+   
+   #seriesInColumns=TRUE, assuming this for now
+
+   if(length(dates) != NROW(data))
+       stop("length of dates not equal length of series.")
+   
+   if(length(ids)   != NCOL(data))
+       stop("number of ids not equal number of series.")
+   
+   if(!is.null(nm)) if(length(nm)   != NCOL(data))
+       stop("number of names not equal number of series.")
+
+   if(!is.null(desc)) if(length(desc)   != NCOL(data))
+       stop("number of descriptions not equal number of series.")
+
+
+   #Adjustments <- c(rep("nsa", 10),rep("sa", 4),rep("nsa", 2)) 
+   #Units    <- z[1, 1]  ; names(Units) <- NULL
+   #Notes    <- z[2, 1]  ; names(Notes) <- NULL
+   #Updated <- z[8, -1]  ; names(Updated) <- NULL# date format error?
+   #Source   <- z[9, -1] ; names(Source) <- NULL
+
+   seriesNames(data ) <- ids
+ 
+   # check that tsrepresentation works
+   z <- try(map$tsrepresentation(data[,1], dates),  silent=TRUE)
+   if(inherits(z, "try-error")) 
+         stop("Could not convert data to series using tsrepresentation.")
+  
+   # cache data, etc in con
+   # use ids to extract from cache, but give names
+   new("TSxlsConnection", drv="xls", dbname=dbname, 
+        hasVintages=FALSE, hasPanels=FALSE, url=url,
+	data=data,ids=ids,dates=dates, names=names, description=desc) 
+   } 
+   )
+
+  # con <- TSconnectXLS(drv="xlsDriver", dbname="d03hist.xls",
+          map=list(ids  =list(i=11,     j="B:Q"), 
+	           data =list(i=12:627, j="B:Q"), 
+	           dates=list(i=12:627, j="A"),
+                   names=list(i=4:7,    j="B:Q"), 
+		   description = NULL,
+		   tsrepresentation = function(data,dates){
+		       ts(data,start=c(1959,7), frequency=12)}))
+
+  # con <- TSconnectXLS(drv="xlsDriver", dbname="d03hist.xls",
+          map=list(ids  =list(i=11,     j="B:Q"), 
+	           data =list(i=12:627, j="B:Q"), 
+	           dates=list(i=12:627, j="A"),
+                   names=list(i=4:7,    j="B:Q"), 
+		   description = NULL,
+		   tsrepresentation = function(data,dates){
+	dt <- strptime(paste("01-",dates[1], sep=""), format="%d-%b-%Y")
+	st <- c(1900+dt$year, dt$mon)
+	ts(data,start=st, frequency=12)}))
+
+  # con <- TSconnectXLS(drv="xlsDriver", dbname="d03hist.xls",
+          map=list(ids  =list(i=11,     j="B:Q"), 
+	           data =list(i=12:627, j="B:Q"), 
+	           dates=list(i=12:627, j="A"),
+                   names=list(i=4:7,    j="B:Q"), 
+		   description = NULL,
+		   tsrepresentation = function(data,dates){
+		       zoo(data,order.by =as.Date(dates))}))
 
 setMethod("TSdates",
   signature(serIDs="character", con="TSxlsConnection", vintage="ANY", panel="ANY"),
@@ -111,4 +206,46 @@ setMethod("TSdoc",   signature(x="character", con="TSxlsConnection"),
 setMethod("TSlabel",   signature(x="character", con="TSxlsConnection"),
    definition= function(x, con=getOption("TSconnection"), ...)
         "TSlabel for TSxls connection not supported." )
+
+##### utilities #####
+
+trimAllNA <- function(x, startNAs= TRUE, endNAs= TRUE) UseMethod("trimAllNA") 
+
+trimAllNA.default <- function(x, startNAs= TRUE, endNAs= TRUE)
+{# trim NAs from the ends of a ts matrix or vector.
+ # (Observations are dropped if all in a given period contain NA.)
+ # if startNAs=F then beginning NAs are not trimmed.
+ # If endNAs=F   then ending NAs are not trimmed.
+ sample <- ! if (is.matrix(x)) apply(is.na(x),1, all) else is.na(x)
+ if (!any(sample)) warning("data is empty after triming NAs.")
+ s <- if (startNAs) min(time(x)[sample]) else tfstart(x)
+ e <- if (endNAs)   max(time(x)[sample]) else tfend(x)
+ tfwindow(x, start=s, end=e, warn=FALSE)
+}
+
+TSrepresentation <- function(df, i, j, tf, names=NULL) {
+   zz <- as.matrix(df)[ i, j]  
+   require("tframe")
+   trimAllNA(tframed(array(as.numeric(zz), dim(zz)), tf=tf, names=names)) 
+   }
+
+tsrepresentation <- function(df, i, j, start, frequency=NULL, names=NULL) {
+   # assume ts with start date string "Month-YYYY" (Locale-specific conversion)
+   dt <- strptime(paste("01-",start, sep=""), format="%d-%b-%Y")
+   # drop NA on end
+   TSrepresentation(df, i, j, 
+      list(start=c(1900+dt$year, dt$mon), frequency=frequency),names=names)
+   }
+
+# dt$year and dt$month in next could be used to construct zoo or other time series
+# dt <- z[ -(1:10), 1]
+# dt <- strptime(paste("01-",dt, sep=""), format="%d-%b-%Y")
+
+combineRows <- function(x, i, j, setEmpty=NULL){
+  x[setEmpty] <- ""
+  x <- x[i,j]
+  r <- NULL
+  for (ii in seq(NROW(x))) r <- paste(r, x[ii,])
+  r
+  }
 
--- pkg/TSxls/tests/Aust.R
+++ pkg/TSxls/tests/Aust.R
@@ -17,48 +17,13 @@ from Int..Money/extracalc
 #  wget "http://www.rba.gov.au/statistics/tables/xls/i02hist.xls"  #has Int CPI
 
 
-trimAllNA <- function(x, startNAs= TRUE, endNAs= TRUE) UseMethod("trimAllNA") 
+TSgetXLS <- function(id, con){
+  # data, ids and dates are cached in con
 
-trimAllNA.default <- function(x, startNAs= TRUE, endNAs= TRUE)
-{# trim NAs from the ends of a ts matrix or vector.
- # (Observations are dropped if all in a given period contain NA.)
- # if startNAs=F then beginning NAs are not trimmed.
- # If endNAs=F   then ending NAs are not trimmed.
- sample <- ! if (is.matrix(x)) apply(is.na(x),1, all) else is.na(x)
- if (!any(sample)) warning("data is empty after triming NAs.")
- s <- if (startNAs) min(time(x)[sample]) else tfstart(x)
- e <- if (endNAs)   max(time(x)[sample]) else tfend(x)
- tfwindow(x, start=s, end=e, warn=FALSE)
-}
-
-TSrepresentation <- function(df, i, j, tf, names=NULL) {
-   zz <- as.matrix(df)[ i, j]  
-   require("tframe")
-   trimAllNA(tframed(array(as.numeric(zz), dim(zz)), tf=tf, names=names)) 
-   }
-
-tsrepresentation <- function(df, i, j, start, frequency=NULL, names=NULL) {
-   # assume ts with start date string "Month-YYYY" (Locale-specific conversion)
-   dt <- strptime(paste("01-",start, sep=""), format="%d-%b-%Y")
-   # drop NA on end
-   TSrepresentation(df, i, j, 
-      list(start=c(1900+dt$year, dt$mon), frequency=frequency),names=names)
-   }
-
-# dt$year and dt$month in next could be used to construct zoo or other time series
-# dt <- z[ -(1:10), 1]
-# dt <- strptime(paste("01-",dt, sep=""), format="%d-%b-%Y")
-
-combineRows <- function(x, i, j, setEmpty=NULL){
-  x[setEmpty] <- ""
-  x <- x[i,j]
-  r <- NULL
-  for (ii in seq(NROW(x))) r <- paste(r, x[ii,])
-  r
+  d <- con@data[,names=Title)
+  length(ids) == NROW(data)
+  length(ids) ==length(dates)
   }
-
-require("gdata")
-
 ####  Australian Money ####
 
 #NB The first line provides data frame names, so rows are shifted.
