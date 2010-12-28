996e889c0874377bd716cc70837a92168bd7d62d
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2010-12-28 02:00:28 +0000

    frb test working (roughly).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@209 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSsdmx/tests/TSgetURI.R
+++ pkg/TSsdmx/tests/TSgetURI.R
@@ -1,33 +1,49 @@
+# TSgetURI is mainly for debugging other TSget functions
 
-this gets the data but the zs parse fails
-#z <- TSgetURI(query="http://sdw.ecb.europa.eu/quickviewexport.do?trans=&start=&end=&snapshot=&periodSortOrder=&SERIES_KEY=122.ICP.M.U2.N.000000.4.ANR&type=sdmx")
+#this gets the data but the zs parse fails
+#z <- TSgetURI(query=
+#"http://sdw.ecb.europa.eu/quickviewexport.do?trans=&start=&end=&snapshot=&periodSortOrder=&SERIES_KEY=122.ICP.M.U2.N.000000.4.ANR&type=sdmx")
 
-we can tell the namespace in effect for the first Series node:
-  xmlNamespace(xmlRoot(z1)[["DataSet"]][[2]])
+#we can tell the namespace in effect for the first Series node:
+#  xmlNamespace(xmlRoot(z1)[["DataSet"]][[2]])
 
-vs
+#vs
 
-this seems to get only the header
-#z <- TSgetURI(query="http://sdw.ecb.europa.eu/export.do?SERIES_KEY=122.ICP.M.U2.N.000000.4.ANR&BS_ITEM=&sfl5=3&sfl4=4&sfl3=4&sfl1=3&DATASET=0&FREQ=M&node=2116082&exportType=sdmx")
+#this seems to get only the header
+#z <- TSgetURI(query=
+#"http://sdw.ecb.europa.eu/export.do?SERIES_KEY=122.ICP.M.U2.N.000000.4.ANR&BS_ITEM=&sfl5=3&sfl4=4&sfl3=4&sfl1=3&DATASET=0&FREQ=M&node=2116082&exportType=sdmx")
 
-# this is the query as per instructions below that work, but the only this does not
-#  diff, as above seems to be that <DataSet> does not have ns inf in <>
-#z <- TSgetURI(query="http://sdw.ecb.europa.eu/export.do?SERIES_KEY=122.ICP.M.U2.N.000000.4.ANR&REF_AREA=308&sfl4=4&sfl3=4&sfl2=4&DATASET=0&ICP_SUFFIX=ANR&node=2120778&exportType=sdmx")
 
- <DataSet xmlns="http://www.ecb.int/vocabulary/stats/bsi" xsi:schemaLocation="http://www.ecb.int/vocabulary/stats/bsi https://stats.ecb.int/stats/vocabulary/bsi/2005-07-01/sdmx-compact.xsd">
-
-# this is the one that works
-#z <- TSgetURI(query="http://sdw.ecb.europa.eu/export.do?SERIES_KEY=117.BSI.Q.U2.N.A.A21.A.1.U2.2250.Z01.E&sfl5=4&sfl4=4&sfl3=4&sfl2=4&sfl1=3&DATASET=0&FREQ=Q&node=2116082&exportType=sdmx")
+# this  works
+#z <- TSgetURI(query=
+#"http://sdw.ecb.europa.eu/export.do?SERIES_KEY=117.BSI.Q.U2.N.A.A21.A.1.U2.2250.Z01.E&sfl5=4&sfl4=4&sfl3=4&sfl2=4&sfl1=3&DATASET=0&FREQ=Q&node=2116082&exportType=sdmx")
 
 # and this monthly version works too (but not right dates from R)
-#z <- TSgetURI(query="http://sdw.ecb.europa.eu/export.do?SERIES_KEY=117.BSI.M.U2.Y.U.A21.A.4.U2.2250.Z01.E&REF_AREA=308&sfl5=3&sfl4=4&sfl3=4&sfl2=4&sfl1=3&DATASET=0&FREQ=M&BS_SUFFIX=E&node=2116082&exportType=sdmx")
+#z <- TSgetURI(query=
+#"http://sdw.ecb.europa.eu/export.do?SERIES_KEY=117.BSI.M.U2.Y.U.A21.A.4.U2.2250.Z01.E&REF_AREA=308&sfl5=3&sfl4=4&sfl3=4&sfl2=4&sfl1=3&DATASET=0&FREQ=M&BS_SUFFIX=E&node=2116082&exportType=sdmx")
 
 
 TSgetURI("http://sdw.ecb.europa.eu/quickviewexport.do?trans=&start=&end=&snapshot=&periodSortOrder=&SERIES_KEY=118.DD.A.I5.POPE.LEV.4D&type=sdmx") #as v3
 
 z <- TSgetURI(query="http://credit.bank-banque-canada.ca/webservices?service=getSeriesSDMX&args=CDOR_-_-_OIS_-_-_SWAPPEDTOFLOAT_-_-_FIRST_-_-_Last")
 
-z <- TSgetURI(query="http://credit.bank-banque-canada.ca/webservices?service=getSeriesSDMX&args=CDOR_-_-_FIRST_-_-_Last")
 
 #Consumer credit from all sources (I think)
-z <- TSgetURI(query="https://www.federalreserve.gov/datadownload/Output.aspx?rel=G19&series=79d3b610380314397facd01b59b37659&lastObs=&from=01/01/1943&to=12/31/2010&filetype=sdmx&label=include&layout=seriescolumn")
+z <- TSsdmx:::TSgetURI(query=
+z <- TSgetURI(query=
+"https://www.federalreserve.gov/datadownload/Output.aspx?rel=G19&series=79d3b610380314397facd01b59b37659&lastObs=&from=01/01/1981&to=12/31/2010&filetype=sdmx&label=include&layout=seriescolumn")
+
+  </message:Header>
+  <frb:DataSet xmlns:kf="http://www.federalreserve.gov/structure/compact/G19_CCOUT" id="CCOUT" xsi:schemaLocation="http://www.federalreserve.gov/structure/compact/G19_CCOUT G19_CCOUT.xsd">
+    <kf:Series CREDTYP="TOTAL" CURRENCY="USD" DATAREP="BILLDOLL" FREQ="129" HOLDER="ALL" SA="SA" SERIES_NAME="DTCTL.M" UNIT="Currency" UNIT_MULT="1000000">
+      <frb:Annotations>
+        <common:Annotation>
+          <common:AnnotationType>Short Description</common:AnnotationType>
+          <common:AnnotationText>Total consumer credit owned and securitized, seasonally adjusted</common:AnnotationText>
+        </common:Annotation>
+        <common:Annotation>
+          <common:AnnotationType>Long Description</common:AnnotationType>
+          <common:AnnotationText>Total consumer credit owned and securitized, seasonally adjusted</common:AnnotationText>
+        </common:Annotation>
+      </frb:Annotations>
+      <frb:Obs OBS_STATUS="A" OBS_VALUE="353029.3" TIME_PERIOD="1981-01-31"/>
--- pkg/TSsdmx/tests/frb.R
+++ pkg/TSsdmx/tests/frb.R
@@ -1,14 +1,16 @@
 #Go through all the steps and at the end there is a link for automated download
 
-Consumer credit from all sources (I think)
-https://www.federalreserve.gov/datadownload/Output.aspx?rel=G19&series=79d3b610380314397facd01b59b37659&lastObs=&from=01/01/1943&to=12/31/2010&filetype=sdmx&label=include&layout=seriescolumn
+#Consumer credit from all sources (I think)
+#https://www.federalreserve.gov/datadownload/Output.aspx?rel=G19&series=79d3b610380314397facd01b59b37659&lastObs=&from=01/01/1943&to=12/31/2010&filetype=sdmx&label=include&layout=seriescolumn
 
 cat("*********** Federal Reserve Board sdmx  ************************\n")
 require("TSsdmx")
 
-con <- TSconnect("sdmx", dbname="BoC") 
+con <- TSconnect("sdmx", dbname="FRB") 
 
-z <- TSget(c("79d3b610380314397facd01b59b37659"), con=con)
+#z <- TSsdmx:::TSgetFRB("79d3b610380314397facd01b59b37659")
+#z <- TSgetFRB("79d3b610380314397facd01b59b37659")
+z <- TSget("79d3b610380314397facd01b59b37659", con=con)
 
 tfplot(z, Title="From Federal Reserve Board")
 TSdescription(z) 
