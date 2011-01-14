6b9f96f7a5c12ae4aa2bd8202fc9c31a84b5ab07
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-14 19:05:02 +0000

    service test for perl modules added.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@220 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSxls/tests/0serviceCheck.R
+++ pkg/TSxls/tests/0serviceCheck.R
@@ -1,1 +1,13 @@
 #not sure what to check here
+
+service <- Sys.getenv("_R_CHECK_HAVE_PERLCSVXS_")
+
+Sys.info()
+
+if(identical(as.logical(service), TRUE)) {
+   require("TSxls") 
+   if( ! "XLSX" %in% xlsFormats()) stop("Perl libraries are not available." )
+ }else {
+   cat("Perl libraries not available. Skipping tests.\n")
+   cat("_R_CHECK_HAVE_PERLCSVXS_ setting ", service, "\n")
+   }
