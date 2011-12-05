d45c9e02e7f160dd90999ee1835cb2564ea1900f
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-05 20:19:40 +0000

    example ets, to clean up still, not working.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@296 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TScompare/R/TScompare.R
+++ pkg/TScompare/R/TScompare.R
@@ -1,6 +1,8 @@
 testequaltf <- function(s1, s2) {
-    r <- try(all( c(earliestEnd(s1) == earliestEnd(s2), 
-	   latestStart(s1) == latestStart(s2))), silent=TRUE)
+    #r <- try(all( c(earliestEnd(s1) == earliestEnd(s2), 
+    #                latestStart(s1) == latestStart(s2))), silent=TRUE)
+    r <- try(all(c(tfend(s1) == tfend(s2), tfstart(s1) == tfstart(s2))), 
+             silent=TRUE)
     if (inherits(r, "try-error")) r <- FALSE # so includes failed as well as unequal
     # failure may be because of different tf representations, 
     #     eg. zoo from sql vs ts from padi for weekly.
@@ -30,11 +32,14 @@ TScompare <- function(ids, con1, con2, na.rm=FALSE, fuzz=1e-14) {
 	      rw[i] <- testequaltf(s1, s2)
 	   
 	      ii <- ! is.na(s1)
+	      #if window is FALSE values are FALSE automatically
 	      if(rw[i]) rv[i] <- all( c(ii == !is.na(s2)) && 
-		                 max(abs(c(s1)[ii] - c(s2)[ii])) < fuzz)
+		                 !any(abs(c(s1)[ii] - c(s2)[ii]) > fuzz))
+	      else rv[i] <- FALSE
 	      }
 	   }
-	r <- list(window=rw, value=rv, ids=ids, na1=na1, na2=na2,con1=con1, con2=con2)
+	r <- list(window=rw, value=rv, ids=ids, na1=na1, na2=na2,
+	         con1=con1, con2=con2, na.rm=na.rm, fuzz=fuzz)
 	class(r) <- "TScompare"
 	r
 	}
@@ -58,7 +63,77 @@ print.summary.TScompare  <- function(x, digits=getOption("digits"), ...){
 	invisible(x)
 	}
 
-tfplot.TScompare  <- function(x, con1, con2, diff=FALSE, ...){
+doubleCheck <- function(x, con1=x$con1, con2=x$con2, na.rm=FALSE, fuzz=1e-10) {
+	# if the con has expired default will fail
+	# go through a TScompare object with a relaxed fuzz and return a new
+	# TScompare object with modified $value
+	# other elements should not be changed
+	if(x$fuzz > fuzz) 
+	   stop("doubleCheck only works if fuzz is bigger than original fuzz.")
+	ids <- x$ids
+	rw  <- x$window
+	rv  <- x$value
+
+	for (i in 1:NROW(ids)){
+	   if((!is.na(rv[i])) && (!rv[i]) && rw[i] ) {
+	   s1 <- try(TSget(ids[i,1], con1), silent=TRUE)
+	   s2 <- try(TSget(ids[i,2], con2), silent=TRUE)
+	   if (inherits(s1, "try-error"))
+	      warning( ids[i,1], "is now NA on con1 (but not change result).")
+	   else if (inherits(s2, "try-error")) 
+	      warning( ids[i,2], "is now NA on con2 (but not change result).")
+	   else {
+	      if(na.rm) {
+		   s1 <- trimNA(s1)
+		   s2 <- trimNA(s2)
+		   }
+	   
+	      ii <- ! is.na(s1)
+	      # only check 
+	      if(rw[i] && !rv[i]) rv[i] <- all( c(ii == !is.na(s2)) && 
+		                 !any(abs(c(s1)[ii] - c(s2)[ii]) > fuzz))
+	      }
+	   }}
+	r <- list(window=rw, value=rv, ids=ids, na1=x$na1, na2=x$na2,
+	       con1=con1, con2=con2, na.rm=na.rm, fuzz=fuzz)
+	class(r) <- "TScompare"
+	r
+	}
+
+tfDetails <- function(x, con1=x$con1, con2=x$con2, na.rm=FALSE) {
+	# if the con has expired default will fail
+	# go through a TScompare object and give details of series with 
+	# different $window
+	ids <- x$ids
+	rw  <- x$window
+	r   <- matrix("ok", NROW(ids), 2)
+	for (i in 1:NROW(ids)){
+	   if(is.na(rw[i]))  r[i,] <- "NA"
+	   if(!rw[i]) {
+	      s1 <- try(TSget(ids[i,1], con1), silent=TRUE)
+	      s2 <- try(TSget(ids[i,2], con2), silent=TRUE)
+	      if (inherits(s1, "try-error"))
+	   	 warning( ids[i,1], "is now NA on con1 (not comparing windows).")
+	      else if (inherits(s2, "try-error")) 
+	   	 warning( ids[i,2], "is now NA on con2 (not comparing windows).")
+	      else {
+	   	 if(na.rm) {
+	   	      s1 <- trimNA(s1)
+	   	      s2 <- trimNA(s2)
+	   	      }
+	      
+	   	 ii <- ! is.na(s1)
+	   	 r[i,1] <- paste(start(s1), " to ", end(s1),collapse="")
+	   	 r[i,2] <- paste(start(s2), " to ", end(s2),collapse="")
+	   	 }
+	      }}
+	dimnames(r) <- list(ids[,1], c("con1", "con2"))
+	r[!rw,]
+	}
+
+tfplot.TScompare  <- function(x, con1=x$con1, con2=x$con2, diff=FALSE, 
+    valueExceptionsOnly=FALSE, ...){
+	# if the con has expired default will fail
 	v <- x$value
 	v[is.na(v)] <- FALSE
 	ids <- x$ids[!(v & x$window),]
