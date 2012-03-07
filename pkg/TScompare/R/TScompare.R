df633daf70f5e3a7c31e3f6f10d67d00986b79e1
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-07 20:49:48 +0000

    minor edit.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@315 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TScompare/R/TScompare.R
+++ pkg/TScompare/R/TScompare.R
@@ -1,6 +1,3 @@
-#   Copyright 2007-2011 Bank of Canada.
-#   Copyright 2012      Paul Gilbert.
-
 testequaltf <- function(s1, s2) {
     #r <- try(all( c(earliestEnd(s1) == earliestEnd(s2), 
     #                latestStart(s1) == latestStart(s2))), silent=TRUE)
