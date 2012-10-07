87ea9002f9254d91a679735bc6440879e115e84b
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-07 22:11:18 +0000

    minor change to a test, bumped version number.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@384 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSPostgreSQL/tests/0serviceCheck.R
+++ pkg/TSPostgreSQL/tests/0serviceCheck.R
@@ -1,48 +1,57 @@
-z <- Sys.getenv("_R_CHECK_HAVE_POSTGRES_")
-
-Sys.info()
-
-if(identical(as.logical(z), TRUE))  require("TSPostgreSQL") else {
-   cat("POSTGRES not available. Skipping tests.\n")
-   cat("_R_CHECK_HAVE_POSTGRES_ setting ", z, "\n")
-   }
-# This just does a dbConnect (not a TSconnect) to see if things work
+# This just uses dbConnect (not TSconnect) to see if things work
 
 service <- Sys.getenv("_R_CHECK_HAVE_POSTGRES_")
 
 Sys.info()
 
-if(identical(as.logical(service), TRUE)) {
+if(!identical(as.logical(service), TRUE)) {
+   cat("POSTGRES not available. Skipping tests.\n")
+   cat("_R_CHECK_HAVE_POSTGRES_ setting ", service, "\n")
+ }else {
    require("TSPostgreSQL") 
    m <- dbDriver("PostgreSQL")
 
    dbname   <- Sys.getenv("POSTGRES_DATABASE")
-   if ("" == dbname)   dbname <-  NULL 
+   if ("" == dbname)  { 
+      dbname <- "test" #PostgreSQL default is template1
+      cat("dbname set to test.\n")
+      }
+   else cat("dbname set to:", dbname, " by env variable POSTGRES_DATABASE\n") 
 
+   host    <- Sys.getenv("POSTGRES_HOST")
+   cat("host set to:", host, " by env variable POSTGRES_HOST\n") 
+   
    user    <- Sys.getenv("POSTGRES_USER")
    if ("" != user) {
+       cat("user set to:", user, " by env variable POSTGRES_USER\n") 
        # specifying host as NULL or "localhost" results in a socket connection
-       host    <- Sys.getenv("POSTGRES_HOST")
-       if ("" == host)     host <- Sys.info()["nodename"] 
+       if ("" == host) {
+          host <- Sys.info()["nodename"]
+          cat("host reset to nodename:", host, "\n") 
+	  } 
        passwd  <- Sys.getenv("POSTGRES_PASSWD")
-       if (is.null(dbname))   dbname <- "test" #RPostgreSQL default is template1
+       cat("passwd set by env variable POSTGRES_PASSWD\n") 
+
        #  See  ?"dbConnect-methods"
        con <- dbConnect(m, dbname=dbname,
           user=user, password=passwd, host=host)  
      }else  {
-        if (is.null(dbname))   dbname <- "test" #RPostgreSQL default is template1
-	#( the postgres driver may also use PGDATABASE, PGHOST, PGPORT, PGUSER )
-	# The Postgress documentation seems to suggest that it should be
-	#   possible to get the host from the .pgpass file too, but I cannot.
-	host <- Sys.getenv("POSTGRES_HOST")
-	if ("" == host) host  <- Sys.getenv("PGHOST")
-	if ("" == host) host  <- "localhost"  #Sys.info()["nodename"] 
-	#get user/passwd in ~/.pgpass
-	con <- dbConnect(m, dbname=dbname, host=host) 
+       cat("user set to empty string.\n") 
+       #(postgres driver may also use PGDATABASE, PGHOST, PGPORT, PGUSER )
+       # The Postgress documentation seems to suggest that it should be
+       #   possible to get the host from the .pgpass file too, but I cannot.
+       
+       if ("" == host) {
+          host <- Sys.getenv("PGHOST")
+          cat("host reset to:", host, "by env variable PGHOST\n") 
+	  } 
+       if ("" == host) {
+          host <- "localhost"  #Sys.info()["nodename"]
+          cat("host reset to:", host, "\n") 
+	  } 
+       #get user/passwd in ~/.pgpass
+       con <- dbConnect(m, dbname=dbname, host=host) 
        }
    # dbListTables(con) needs a non-null dbname
    dbDisconnect(con)
- }else {
-   cat("POSTGRES not available. Skipping tests.\n")
-   cat("_R_CHECK_HAVE_POSTGRES_ setting ", service, "\n")
-   }
+ }
