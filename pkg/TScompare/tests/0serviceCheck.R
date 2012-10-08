5a0175f2c386c176ee24232de8b054b7220faf20
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2012-10-08 21:54:27 +0000

    Removed use of TSpadi and put in TSSQLite instaed. Cleaned up testing to record more details of how the connection parameters are determined, and removed old tests of ets. One test should be switched to TSjson when that is working.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@385 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/TScompare/tests/0serviceCheck.R
@@ -0,0 +1,94 @@
+Sys.info()
+
+if(!identical(as.logical(Sys.getenv("_R_CHECK_HAVE_MYSQL_")), TRUE)) {
+   cat("MYSQL not available. Skipping tests.\n")
+   cat("_R_CHECK_HAVE_MYSQL_ setting ", service1, "\n")
+ }else if(!identical(as.logical(Sys.getenv("_R_CHECK_HAVE_SQLITE_")), TRUE)) {
+   cat("SQLLITE  not available. Skipping tests.\n")
+   cat("_R_CHECK_HAVE_SQLLITE_ setting ", service2, "\n")
+ } else  
+   {
+   require("TScompare")
+   require("TSMySQL")
+   require("TSSQLite")
+
+   user1    <- Sys.getenv("MYSQL_USER")
+   cat("user1 set to:", user1, " by env variable MYSQL_USER\n") 
+   if ("" != user1) {
+       # specifying host as NULL or "localhost" results in a socket connection
+       host1    <- Sys.getenv("MYSQL_HOST")
+       if ("" == host1)     host1 <- Sys.info()["nodename"] 
+       passwd1  <- Sys.getenv("MYSQL_PASSWD")
+       if ("" == passwd1)   passwd1 <- NULL
+       }
+    
+   cat("****************************************************************\n")
+   cat("WARNING: THIS OVERWRITES TABLES IN TEST DATABASE ON MySQL SERVER\n")
+   cat("****************************************************************\n")
+
+   if ("" != user1) con <- try(dbConnect("MySQL", dbname="test", 
+	                 username=user1, password=passwd1, host=host1)) 
+   else  con <- try(dbConnect("MySQL", dbname="test"))#user/passwd/host .my.cnf
+ 
+   if (inherits(con, "try-error")) stop("dbConnect to MySQL db test failed./n")
+ 
+   dbListTables(con) 
+   source(system.file("TSsql/CreateTables.TSsql", package = "TSdbi"))
+   dbListTables(con) 
+   dbDisconnect(con)
+
+   if ("" != user1) con1 <- try(TSconnect("MySQL", dbname="test", 
+	                 username=user1, password=passwd1, host=host1)) 
+   else  con1 <- try(TSconnect("MySQL", dbname="test"))#user/passwd/host .my.cnf
+   if (inherits(con1, "try-error")) 
+        stop("TSconnect to MySQL db test failed./n")
+ 
+
+   cat("*******************************************************************\n")
+   cat("* WARNING: THIS OVERWRITES TABLES IN TEST DATABASE ON RSQLite SERVER\n")
+   cat("*******************************************************************\n")
+
+   con <- try(dbConnect("SQLite", dbname="test")) # no user/passwd/host
+   if (inherits(con, "try-error"))
+           stop("dbConnect to SQLite db test failed./n")
+
+   dbListTables(con) 
+   source(system.file("TSsql/CreateTables.TSsql", package = "TSdbi"))
+   dbListTables(con) 
+   dbDisconnect(con)
+
+   con2 <- try(TSconnect("SQLite", dbname="test")) # no user/passwd/host
+   if (inherits(con2, "try-error"))
+           stop("TSconnect to SQLite db test failed./n")
+
+#   user2    <- Sys.getenv("POSTGRES_USER")
+#   cat("user2 set to:", user2, " by env variable POSTGRES_USER\n") 
+#   
+#   host2    <- Sys.getenv("POSTGRES_HOST")
+#   cat("host2 set to:", host2, " by env variable POSTGRES_HOST\n") 
+#     } else  {
+#       cat("user2 set to empty string.\n") 
+#       #(postgres driver may also use PGDATABASE, PGHOST, PGPORT, PGUSER )
+#       # The Postgress documentation seems to suggest that it should be
+#       #   possible to get the host from the .pgpass file too, but I cannot.
+#       
+#       if ("" == host2) {
+#          host2 <- Sys.getenv("PGHOST")
+#          cat("host2 reset to:", host2, "by env variable PGHOST\n") 
+#	  } 
+#       if ("" == host2) {
+#          host2 <- "localhost"  #Sys.info()["nodename"]
+#          cat("host2 reset to:", host2, "\n") 
+#	  } 
+#       con2 <- try(TSconnect("PostgreSQL", dbname="test"))#user/pwd/host in cfg #
+#       if (inherits(con2, "try-error"))
+#           stop("connection to PostgreSQL db test failed./n")
+#       }
+
+   
+   cat("*** TSconnect connections established. Disconnecting\n")
+  
+   dbDisconnect(con1)
+   dbDisconnect(con2)
+ 
+}
--- pkg/TScompare/tests/0wfsCheck.R
+++ pkg/TScompare/tests/0wfsCheck.R
@@ -1,16 +1,11 @@
-service1 <- Sys.getenv("_R_CHECK_HAVE_MYSQL_")
-service2 <- Sys.getenv("_R_CHECK_HAVE_PADI_")
+Sys.info()
 
-if(!identical(as.logical(service1), TRUE) |
-   !identical(as.logical(service2), TRUE)) {
-   cat("PADI or MYSQL not available. Skipping tests.\n")
+if(!identical(as.logical(Sys.getenv("_R_CHECK_HAVE_MYSQL_")), TRUE)) {
+   cat("MYSQL not available. Skipping tests.\n")
    cat("_R_CHECK_HAVE_MYSQL_ setting ", service1, "\n")
-   cat("_R_CHECK_HAVE_PADI_ setting ", service2, "\n")
-} else  {
-
+ } else {
    require("TScompare")
    require("TSMySQL")
-   require("TSpadi")
 
    user1    <- Sys.getenv("MYSQL_USER")
    if ("" != user1) {
@@ -21,42 +16,36 @@ if(!identical(as.logical(service1), TRUE) |
        if ("" == passwd1)   passwd1 <- NULL
      }
 
-   con1 <- if ("" != user1)  
-            try(TSconnect("MySQL", dbname="wfs", 
+   if ("" != user1){
+      con1 <- try(TSconnect("MySQL", dbname="wfsv", 
 	        username=user1, password=passwd1, host=host1), silent=TRUE) 
-      else  try(TSconnect("MySQL", dbname="wfs"), silent=TRUE) # pass user/passwd/host in ~/.my.cnf
+      con2 <- try(TSconnect("MySQL", dbname="ets", 
+	        username=user1, password=passwd1, host=host1), silent=TRUE) 
+   }else {
+      con1 <- try(TSconnect("MySQL", dbname="wfsv"), silent=TRUE)#use~/.my.cnf
+      con2 <- try(TSconnect("MySQL", dbname="ets"),  silent=TRUE)#use~/.my.cnf
+      }
 
-
-   user2    <- Sys.getenv("PADI_USER")
-   if ("" != user2) {
-       # specifying host as NULL or "localhost" results in a socket connection
-       host2    <- Sys.getenv("MYSQL_HOST")
-       if ("" == host2)     host2 <- Sys.info()["nodename"] 
-       passwd2  <- Sys.getenv("MYSQL_PASSWD")
-       if ("" == passwd2)   passwd2 <- NULL
-     }
-    
-   con2 <-  if ("" != user2) 
-            try(TSconnect("padi", dbname="ets",
-	         username=user, password=passwd, host=host2), silent=TRUE) 
-       else try(TSconnect("padi", dbname="ets"), silent=TRUE) # pass user/passwd/host in ~/.padi.cfg
 
    if      (inherits(con1, "try-error"))
            cat("wfs connection not available. Skipping 0wfsCheck tests.")
    else if (inherits(con2, "try-error"))
            cat("ets connection not available. Skipping 0wfsCheck tests.")
    else {
-      ids <- AllIds(con1)
-      if(!is.null(AllPanels(con1)))   stop("Bad result. wfs does not have panels.")
-      if( is.null(AllVintages(con1))) stop("Bad result. wfs has vintages.")
+      ids <- AllIds(con1)[1:100] #4677 in 2011-05-13
+      if(!is.null(AllPanels(con1)))
+              stop("Bad result. wfsv does not have panels.")
+      if( is.null(AllVintages(con1))) 
+              stop("Bad result. wfsv has vintages.")
+
       ids <- cbind(ids, ids)
       eq   <- TScompare(ids, con1, con2, na.rm=FALSE)
       print(summary(eq))
       eqrm <- TScompare(ids, con1, con2, na.rm=TRUE)
       print(summary(eqrm))
   
-      cat("**************        disconnecting ets\n")
+      cat("**************        disconnecting.\n")
       dbDisconnect(con1)
-      #dbDisconnect(con2)
+      dbDisconnect(con2)
       }
 }
