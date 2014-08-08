e09e60926714a7e15d6acdbcd6a83472a0185aff
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-08 16:35:17 +0000

    minor edit.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@510 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSsdmx/tests/bis.R
+++ pkg/TSsdmx/tests/bis.R
@@ -6,6 +6,18 @@
 
 # http://stats.bis.org/
 
+## python playing
+## import requests
+## get_response = requests.get(url='http://google.com')
+## get_response = requests.get(url='http://stats.bis.org/bis-stats-tool/org.bis.stats.ui.StatsApplication/ResultDownload?format=sdmx-ml-data&bookmark=true&query=eJwtzMENwjAQBMC1FUHCC1EHTZzNWiAIkLNRlJc7oLGUQgdUw6FkH7e6eSzgjp93HbAPlzwy1CglxzN74rAKqScpkm6PEZYO2M5rf603ztlt0CpTFaXY5%2BGjwMsLTVIOJg6ux86mapB8ZVnojvZPZXpyAcUP9e8ZAQ%3D%3D')
+
+## get_response.text
+
+
+## post_data = {'username':'joeb', 'password':'foobar'}
+## post_response = requests.post(url='http://some.other.site', data=post_data)
+
+
 #> Statistics > Display Query URL
  
 #http://stats.bis.org/bis-stats-tool/org.bis.stats.ui.StatsApplication/ResultDownload?format=csv&bookmark=true&query=eJxjYGDUOZ1iXcUg4OQZHO7qFO%2FsGBLs7OHq68ogBBVxdQ1ycQxxdPPxD2cAAk4GBvbVUPoWkGZjZASSjAwsbkGugWAWoy8DAD7ID1Q%3D
