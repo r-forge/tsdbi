0ebc651f79e9c933e52a445d838349739d6e677d
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2014-12-13 01:55:16 +0000

    adding Guide.pdf. Package buils but not well checked yet.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@558 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSsdmx/inst/testWithInternet/abs.R
+++ pkg/TSsdmx/inst/testWithInternet/abs.R
@@ -19,9 +19,9 @@ if   (4 != frequency(z))           stop("abs test 1 frequency failure.")
 
 ##################  monthly #################
 
-z <- TSget('RT.0.2.15.10.M', start=c(1990, 1), end=c(2012, 2), abs) 
-names(tts)
+z <- TSget('RT.0.2.15.10.M', start=c(1990, 1), end=c(2012, 2), names="Retail Trade", abs) 
 
+if ("Retail Trade" != seriesNames(z))    stop("abs test 2 seriesName failure.")
 if (! all(c(1990, 1) == start(z))) stop("abs test 2 start date failure.")
 if (! all(c(2012, 2) == end(z)))   stop("abs test 2  end  date failure.")
 if   (12 != frequency(z))          stop("abs test 2 frequency failure.")
