378727a1f70d9011ecd7fe20d48dbb3889175559
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-29 23:29:45 +0000

    Fixed bug passing server arguments in TSconnect, which was only using the driver configuration file and not any specified arguments. Bumped version number.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsdbi@629 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TSdbi/R/TSdbi.R
+++ pkg/TSdbi/R/TSdbi.R
@@ -92,10 +92,10 @@ setGeneric("TSmeta<-",
 
 setGeneric("TSconnect", def= function(q, dbname, ...) standardGeneric("TSconnect"))
 
-# note that ... is passed to both the driver and to TSconnect in next
+# note that ... is passed to both the driver and to dbConnect in next
 setMethod("TSconnect",   signature(q="character", dbname="character"),
    definition=function(q, dbname, ...)
-        TSconnect(dbConnect(get(q, mode="function")(...), dbname=dbname), ... ))
+        TSconnect(dbConnect(get(q, mode="function")(...), dbname=dbname, ... )))
 
 # This would work above for DBI sql engines, but for others the TS* package needs to export
 #        TSconnect(dbConnect(getExportedValue(paste("R", q,sep=""), q)(),
